cb5a55eb55bca30bfacf1183c78b823340a4427b
jackal7819 <jackal3166@gmail.com>
2025-10-20 16:25:35 +0300

    feat: add Results component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Logo from './Logo';
+import Results from './Results';
 import Search from './Search';
 
 export default function Navbar() {
@@ -6,9 +7,7 @@ export default function Navbar() {
 		<nav className='container flex flex-wrap items-center justify-between gap-10 p-10 mx-auto bg-indigo-500 lg:gap-20 w-9/10 rounded-xl'>
 			<Logo />
 			<Search />
-			<p>
-				Found <strong>5</strong> results
-			</p>
+			<Results />
 		</nav>
 	);
 }
---
+++ src/components/Results.tsx
@@ -0,0 +1,7 @@
+export default function Results() {
+	return (
+		<p>
+			Found <strong>5</strong> results
+		</p>
+	);
+}
