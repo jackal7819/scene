e4fade055004318a133dde7d9f158b07d307ebc0
jackal7819 <jackal3166@gmail.com>
2025-10-20 17:10:08 +0300

    feat: add WatchedMoviesList component

---
+++ src/components/WatchedMoviesList.tsx
@@ -0,0 +1,37 @@
+interface WatchedMoviesListProps {
+	watched: {
+		imdbID: string;
+		Poster: string;
+		Title: string;
+		imdbRating: number;
+		userRating: number;
+		runtime: number;
+	}[];
+}
+
+export default function WatchedMoviesList({ watched }: WatchedMoviesListProps) {
+	return (
+		<ul>
+			{watched.map((movie) => (
+				<li key={movie.imdbID}>
+					<img src={movie.Poster} alt={`${movie.Title} poster`} />
+					<h3>{movie.Title}</h3>
+					<div>
+						<p>
+							<span>⭐️</span>
+							<span>{movie.imdbRating}</span>
+						</p>
+						<p>
+							<span>🌟</span>
+							<span>{movie.userRating}</span>
+						</p>
+						<p>
+							<span>⏳</span>
+							<span>{movie.runtime} min</span>
+						</p>
+					</div>
+				</li>
+			))}
+		</ul>
+	);
+}
