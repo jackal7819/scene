2556ef1c6bc823a8895659a429512926722e9725
jackal7819 <jackal3166@gmail.com>
2025-10-20 16:22:40 +0300

    feat: add Search component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+	return (
+		<div className='flex items-center gap-10'>
+			<span role='img' title='logo' className='text-5xl'>
+				🎬
+			</span>
+			<h1 className='text-3xl uppercase'>Scene</h1>
+		</div>
+	);
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,10 @@
+import Logo from './Logo';
 import Search from './Search';
 
 export default function Navbar() {
 	return (
 		<nav className='container flex flex-wrap items-center justify-between gap-10 p-10 mx-auto bg-indigo-500 lg:gap-20 w-9/10 rounded-xl'>
-			<div className='flex items-center gap-10'>
-				<span role='img' title='logo' className='text-5xl'>
-					🎬
-				</span>
-				<h1 className='text-3xl uppercase'>Scene</h1>
-			</div>
+			<Logo />
 			<Search />
 			<p>
 				Found <strong>5</strong> results
