3f5025c004c5175ce1adba55f21af6bb432d4032
jackal7819 <jackal3166@gmail.com>
2025-10-20 17:17:05 +0300

    feat: add WatchedMovie component

--- src/components/WatchedMoviesList.tsx
+++ src/components/WatchedMoviesList.tsx
@@ -1,3 +1,5 @@
+import WatchedMovie from './WatchedMovie';
+
 interface WatchedMoviesListProps {
 	watched: {
 		imdbID: string;
@@ -13,24 +15,7 @@ export default function WatchedMoviesList({ watched }: WatchedMoviesListProps) {
 	return (
 		<ul>
 			{watched.map((movie) => (
-				<li key={movie.imdbID}>
-					<img src={movie.Poster} alt={`${movie.Title} poster`} />
-					<h3>{movie.Title}</h3>
-					<div>
-						<p>
-							<span>⭐️</span>
-							<span>{movie.imdbRating}</span>
-						</p>
-						<p>
-							<span>🌟</span>
-							<span>{movie.userRating}</span>
-						</p>
-						<p>
-							<span>⏳</span>
-							<span>{movie.runtime} min</span>
-						</p>
-					</div>
-				</li>
+				<WatchedMovie key={movie.imdbID} movie={movie} />
 			))}
 		</ul>
 	);
