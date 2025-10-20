a5c9d2d2dd8c742e1ede74c3e454c8ece4a7dfd9
jackal7819 <jackal3166@gmail.com>
2025-10-20 16:33:57 +0300

    feat: add WatchedBox component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,72 +1,11 @@
-import { useState } from 'react';
-
 import ListBox from './ListBox';
-import { average, tempMovieData, tempWatchedData } from '../../constants';
+import WatchedBox from './WatchedBox';
 
 export default function Main() {
-	const [watched, setWatched] = useState(tempWatchedData);
-	const [isOpen2, setIsOpen2] = useState(true);
-
-	const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-	const avgUserRating = average(watched.map((movie) => movie.userRating));
-	const avgRuntime = average(watched.map((movie) => movie.runtime));
-
 	return (
 		<main className='container flex flex-grow p-10 mx-auto w-9/10'>
 			<ListBox />
-			<div>
-				<button type='button' onClick={() => setIsOpen2((prev) => !prev)}>
-					{isOpen2 ? '–' : '+'}
-				</button>
-				{isOpen2 && (
-					<>
-						<div>
-							<h2>Movies you watched</h2>
-							<div>
-								<p>
-									<span>#️⃣</span>
-									<span>{watched.length}</span>
-								</p>
-								<p>
-									<span>⭐️</span>
-									<span>{avgImdbRating}</span>
-								</p>
-								<p>
-									<span>🌟</span>
-									<span>{avgUserRating}</span>
-								</p>
-								<p>
-									<span>⏳</span>
-									<span>{avgRuntime} min</span>
-								</p>
-							</div>
-						</div>
-
-						<ul>
-							{watched.map((movie) => (
-								<li key={movie.imdbID}>
-									<img src={movie.Poster} alt={`${movie.Title} poster`} />
-									<h3>{movie.Title}</h3>
-									<div>
-										<p>
-											<span>⭐️</span>
-											<span>{movie.imdbRating}</span>
-										</p>
-										<p>
-											<span>🌟</span>
-											<span>{movie.userRating}</span>
-										</p>
-										<p>
-											<span>⏳</span>
-											<span>{movie.runtime} min</span>
-										</p>
-									</div>
-								</li>
-							))}
-						</ul>
-					</>
-				)}
-			</div>
+			<WatchedBox />
 		</main>
 	);
 }
