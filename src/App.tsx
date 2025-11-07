448165c61ffbc23367d7dda6bbff908a0ef5dfde
jackal7819 <jackal3166@gmail.com>
2025-11-07 12:40:22 +0200

    feat: add getMovies function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import ListBox from './components/ListBox';
 import Logo from './components/Logo';
@@ -7,14 +7,31 @@ import MoviesList from './components/MoviesList';
 import Navbar from './components/Navbar';
 import Results from './components/Results';
 import Search from './components/Search';
-import WatchedMoviesList from './components/WatchedMoviesList'
-import WatchedSummary from './components/WatchedSummary'
+import WatchedMoviesList from './components/WatchedMoviesList';
+import WatchedSummary from './components/WatchedSummary';
 import type { IMovie } from './components/Movie';
-import { tempMovieData, tempWatchedData } from '../constants';
+
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 
 export default function App() {
-	const [movies, setMovies] = useState<IMovie[]>(tempMovieData);
-	const [watched, setWatched] = useState(tempWatchedData);
+	const [movies, setMovies] = useState<IMovie[]>([]);
+	const [watched, setWatched] = useState<IMovie[]>([]);
+
+	const getMovies = async (query: string) => {
+		try {
+			const url = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`;
+			const res = await fetch(url);
+			const data = await res.json();
+			setMovies(data.Search);
+			setWatched([]);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		getMovies('Batman');
+	}, []);
 
 	return (
 		<div className='min-h-screen py-10 text-xl text-white bg-slate-800'>
