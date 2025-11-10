19a93d2feb2f084e0cfa0a17f43359febfb483a6
jackal7819 <jackal3166@gmail.com>
2025-11-10 13:06:43 +0200

    feat: add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import ListBox from './components/ListBox';
+import Loader from './components/Loader';
 import Logo from './components/Logo';
 import Main from './components/Main';
 import MoviesList from './components/MoviesList';
@@ -12,18 +13,22 @@ import WatchedSummary from './components/WatchedSummary';
 import type { IMovie } from './components/Movie';
 
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const API_BASE_URL = import.meta.env.VITE_OMDB_BASE_URL;
 
 export default function App() {
 	const [movies, setMovies] = useState<IMovie[]>([]);
 	const [watched, setWatched] = useState<IMovie[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const getMovies = async (query: string) => {
 		try {
-			const url = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`;
+			setIsLoading(true);
+			const url = `${API_BASE_URL}/?apikey=${API_KEY}&s=${query}`;
 			const res = await fetch(url);
 			const data = await res.json();
 			setMovies(data.Search);
 			setWatched([]);
+			setIsLoading(false);
 		} catch (error) {
 			console.error(error);
 		}
@@ -41,9 +46,7 @@ export default function App() {
 				<Results movies={movies} />
 			</Navbar>
 			<Main>
-				<ListBox>
-					<MoviesList movies={movies} />
-				</ListBox>
+				<ListBox>{isLoading ? <Loader /> : <MoviesList movies={movies} />}</ListBox>
 				<ListBox>
 					<WatchedSummary watched={watched} />
 					<WatchedMoviesList watched={watched} />
---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+	return (
+		<div className="flex items-center justify-center w-full h-full">
+			<h2>LOADING...</h2>
+		</div>
+	);
+}
